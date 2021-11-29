43d5e3e481bc56e6928ce0762a27f853f93af21e
GianfrancoJara <gia.jara@duocuc.cl>
2021-11-29 00:15:51 -0300

    Modificacion de pruebas

--- e2e/src/pagePrueba.e2e-spec.ts
+++ e2e/src/pagePrueba.e2e-spec.ts
@@ -1,15 +1,19 @@
 import {browser, element, by } from 'protractor';
  
-describe('Titulo prueba 1.', ()=>{
+describe('Pruebas.', ()=>{
     //configuramos nuestro bloque
     beforeEach(()=>{
         browser.get("/");
 
     });
-    //creación de prueba 1
-    it("El titulo se muestra correctamente", ()=>{
+    //creación de pruebas
+    it("Prueba 1: El titulo se muestra correctamente", ()=>{
         expect(element(by.css("#titulokkk ion-title")).getText()).toContain("InfoGaming");
-    });  
+    });
+    
+    it("Prueba 2: El label se muestra correctamente", ()=>{
+        expect(element(by.css("#prueba2 ion-label")).getText()).toContain("NOVEDADES GAMING");
+    }); 
 
 });
 
